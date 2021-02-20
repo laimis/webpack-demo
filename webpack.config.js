3284d3873e30f167bf06e045c553b4dd818192b7
Laimonas Simutis <lsimutis@mediafly.com>
2021-02-20 09:41:38 -0500

    playing with fonts

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,14 @@ module.exports = {
             'css-loader'
         ],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
 };

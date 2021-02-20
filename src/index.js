a8cfef1858020a8e386c431ff5474370413cd86e
Laimonas Simutis <lsimutis@mediafly.com>
2021-02-20 09:52:26 -0500

    loading json data

--- src/index.js
+++ src/index.js
@@ -4,21 +4,25 @@ import './style.css';
 
 import Icon from './icon.png';
 
+import Data from './data.json';
+
 
 function component() {
-  const element = document.createElement("div");
+    const element = document.createElement("div");
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(["Why would I want to create everything in javascript, lol", "webpack"], " ");
-  element.classList.add('hello');
+    // Lodash, now imported by this script
+    element.innerHTML = _.join(["Why would I want to create everything in javascript, lol", "webpack"], " ");
+    element.classList.add('hello');
 
-   // Add the image to our existing div.
-   const myIcon = new Image();
-   myIcon.src = Icon;
- 
-   element.appendChild(myIcon);
+    // Add the image to our existing div.
+    const myIcon = new Image();
+    myIcon.src = Icon;
+    
+    element.appendChild(myIcon);
 
-  return element;
+    Data.daters.forEach(e => console.log(e.a));
+
+    return element;
 }
 
 document.body.appendChild(component());
